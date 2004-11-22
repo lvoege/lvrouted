31f7ee6456626e0ea04c68ec92e2bca76d3274be
lodewijk <lodewijk@f2f20d11-44c1-0310-b3a0-a868ae65eea2>
2004-11-22 03:11:34 +0000

    r5379@polaris:  lodewijk | 2004-11-22T03:11:57.909003Z   r5370@polaris (orig r1833):   | 2004-11-21T02:50:10.353863Z   laat LowLevel.get_arp_entries een list ipv een array teruggeven, dat versimpelt de C code nogal.

--- node-config/other/lvrouted/trunk/LowLevel.ml
+++ node-config/other/lvrouted/trunk/LowLevel.ml
@@ -60,7 +60,7 @@ external get_addrs_in_block: Unix.inet_addr -> int -> Unix.inet_addr list
 external get_arp_entries: unit -> ( string		(* iface name *)
 				  * Unix.inet_addr	(* IPv4 addr *)
 				  * string)		(* MAC.t *)
-				  array
+				  list
   = "get_arp_entries"
 
 (* Get all the MAC.t's that are associated with the interface with the given 
