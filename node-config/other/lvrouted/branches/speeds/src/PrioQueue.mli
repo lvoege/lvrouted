6edcbc4aeb0948e8768144d5aa1ff06a7e70c975
lodewijk <lodewijk@f2f20d11-44c1-0310-b3a0-a868ae65eea2>
2005-07-21 22:45:04 +0000

    r7171@polaris:  lodewijk | 2005-07-22 00:44:22 +0200   r7167@polaris (orig r3824):  (no author) | 2005-07-21 22:47:37 +0200   cleanup

--- node-config/other/lvrouted/branches/speeds/src/PrioQueue.mli
+++ node-config/other/lvrouted/branches/speeds/src/PrioQueue.mli
@@ -1,22 +1,21 @@
-module type OrderedType =
-  sig type priority val compare : priority -> priority -> int end
-module type S =
-  sig
-    type priority
-    type 'a queue
-    val empty : 'a queue
-    val insert : 'a queue -> priority -> 'a -> 'a queue
-    exception Queue_is_empty
-    val extract : 'a queue -> priority * 'a * 'a queue
-  end
-module Make :
-  functor (P : OrderedType) ->
-    sig
-      type priority = P.priority
-      type 'a queue = Empty | Node of priority * 'a * 'a queue * 'a queue
-      val empty : 'a queue
-      val insert : 'a queue -> priority -> 'a -> 'a queue
-      exception Queue_is_empty
-      val remove_top : 'a queue -> 'a queue
-      val extract : 'a queue -> priority * 'a * 'a queue
-    end
+(* This is a priority queue functorized so you can plug in any type you
+   can define a comparison operator for. For example:
+
+   module IntQueue = PrioQueue.Make(struct
+   	type priority = int
+	let compare = compare
+   end)
+
+   will give you a queue that has integer priority *)
+module type OrderedType = sig
+	type priority 
+	val compare : priority -> priority -> int
+end
+module Make : functor (P : OrderedType) -> sig
+	type priority = P.priority
+	type 'a queue
+	val empty : 'a queue
+	val insert : 'a queue -> priority -> 'a -> 'a queue
+	exception Queue_is_empty
+	val extract : 'a queue -> priority * 'a * 'a queue
+end
