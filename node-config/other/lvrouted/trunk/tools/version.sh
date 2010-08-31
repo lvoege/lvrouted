0b6d9a089ef1ce1ae09bb1a0c934717b8dd44d7b
lodewijk <lodewijk@f2f20d11-44c1-0310-b3a0-a868ae65eea2>
2010-08-31 14:09:22 +0000

    not using SVK anymore

--- node-config/other/lvrouted/trunk/tools/version.sh
+++ node-config/other/lvrouted/trunk/tools/version.sh
@@ -8,11 +8,6 @@ elif [ -e ".svn/entries" ]; then
 	echo Extracting svn version info
 	echo let version=`svn info . | grep Revision | sed "s/.* //g"` >> Version.ml
 	echo let branch=\"`svn info . | grep URL | sed "s/.*lvrouted\///g" | sed "s/\/src//g"`\" >> Version.ml
-else
-	echo Extracting svk version info
-	echo let version=`svk info . | grep Mirrored | sed "s/.*Rev. //g"` >> Version.ml
-	echo let branch=\"`svk info . | grep Depot | sed "s/.*lvrouted\///g" | sed "s/\/src//g"`\" >> Version.ml
-		
 fi
 echo let date=\"`date`\" >> Version.ml
 echo let host=\"`uname -a`\" >> Version.ml
