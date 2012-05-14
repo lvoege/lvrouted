d6d7f15bfecf420d4d62094c3226a0051e40756f
lodewijk <lvoege@gmail.com>
2012-05-14 14:19:56 +0000

    merge -r 10728:HEAD van ../branches/pq, want deze eerste kleine stap lijkt te werken. dit vervangt de normale lineaire queue waarmee de breadth-first traversal van de boom gedaan wordt door een priority queue. en het gooit ook wat eeuwen-oude code weg uit lowlevel_c.c.

--- src/test.ml
+++ src/test.ml
@@ -11,30 +11,25 @@ let read_packet s =
 let _ =
 	let trees = List.fold_left (fun a em ->
 		(read_packet ("dump/lvrouted.packet-172." ^ em))::a) [] [
-		"16.0.33";
-		"16.0.42";
-		"16.0.58";
-		"16.0.78";
-		"16.5.1";
-		"16.8.30";
-		"17.143.1";
-		"17.143.3";
-		"17.143.4";
+		"16.4.121";
+		"16.4.25";
+		"16.4.9";
 	] in
+	let foo = Tree.merge trees [] in
+	let nodes, routes = foo in
+	Gc.compact ();
+	print_string (Tree.show nodes);
+	Gc.compact ();
+	print_newline ()
+(*
 	let a = Array.make 1001 (Tree.merge trees []) in
 	for i = 0 to 1000 do
 		Gc.compact ();
 		let trees = List.fold_left (fun a em ->
 			(read_packet ("dump/lvrouted.packet-172." ^ em))::a) [] [
-			"16.0.33";
-			"16.0.42";
-			"16.0.58";
-			"16.0.78";
-			"16.5.1";
-			"16.8.30";
-			"17.143.1";
-			"17.143.3";
-			"17.143.4";
+			"16.4.121";
+			"16.4.25";
+			"16.4.9";
 		] in
 		Gc.compact ();
 		let foo = Tree.merge trees [] in
@@ -45,3 +40,4 @@ let _ =
 		Gc.compact ();
 		print_newline ()
 	done
+	*)
