f9d5cc301cc0873df061fbd3b761ad592013bd26
lodewijk <lvoege@gmail.com>
2012-06-29 02:51:15 +0000

    ticket #252: attempt to keep routes to nodes for which multiple paths of equal cost exist stable. for now, by having the gateway with the numerically lower address win.

--- src/Tree.ml
+++ src/Tree.ml
@@ -57,24 +57,34 @@ let dump_tree fname nodes =
    own addresses, return the spanning tree for this node, plus a routing
    table.
    
-   1. Initialize a routing table with routes to our own addresses.
-   2. Make a new node to hang the new, merged and pruned tree under
-   3. Traverse the tree breadth-first.  For every node, check if
-      there is a route already. If so, produce no new node. If not,
-      add a route and create a new node and prepend it to the parent's
-      list of children.
-   4. Filter out routes that are included in a route from the
-      list of directly attached routes. This may not be necessary anymore, but
-      it was when route addition didn't work correctly.
+   1. Initialize a routing table with routes to our own addresses. This is
+      a mapping from address to a pair of cost and gateway.
+   2. Reserve a slot to put the default gateway in. This would be the first
+      address we see during the tree merge that is indeed marked as a valid
+      gateway.
+   3. Make a new node to hang the new, merged and pruned tree under.
+   4. Traverse the tree breadth-first.  For every node, check if
+      there is a route already. If there isn't, add a route and create a new
+      node and prepend it to the parent's list of children.
 
-   The traversal routine takes a routing table and a list of tuples to
-   process. Those tuples are of the form (node, parent, gateway address).
-   If the node isn't already in the routing table:
-     - a new node is produced and hooked under the parent
-     - the new node is inserted into the routing table
-     - the original node's children are appended to the list of tuples to
-       process, with the new node as the parent entry. the gateway argument
-       stays the same.
+      On the other hand, if there is already a route, check the cost (==
+      priority) of that route. If it's less than the cost of the node we're
+      looking at, ignore this node and continue traversing because we
+      evidently already have a route to the node's address and it's strictly
+      better. If it's more than the cost of the current node, panic because
+      that shouldn't happen (that's the point of the priority queue after
+      all).
+      
+      If it's equal, have the gateway that has the numerically lowest address
+      of the two win. This will keep routes to addresses for which there are
+      multiple equally costly paths stable. Also update the default gateway
+      if necessary.
+   5. From the routing table that maps addresses to pairs of cost and gateway,
+      construct one that maps addresses to just the gateway, because the
+      caller doesn't care about cost. While doing that, filter out routes that
+      are included in a route from the list of directly attached routes. This
+      may not be necessary anymore, but it was when route addition didn't work
+      correctly.
 
    Note that the resulting spanning tree is returned as the list of
    first-level nodes, because the top node is relevant only to the
@@ -85,47 +95,55 @@ let dump_tree fname nodes =
 let merge nodes directnets =
 	(* step 1 *)
 	let routes = IPHash.create 512 in
-	List.iter (fun (a, _) -> IPHash.add routes a a) directnets;
-	let default_gw = ref Unix.inet_addr_any in
+	List.iter (fun (a, _) -> IPHash.add routes a (0, a)) directnets;
 	(* step 2 *)
-	let fake = make Unix.inet_addr_any true false [] in
+	let default_gw = ref Unix.inet_addr_any in
 	(* step 3 *)
+	let fake = make Unix.inet_addr_any true false [] in
+	(* step 4 *)
 	let rec traverse pq =
 		if pq = IntQueue.empty then ()
 		else    let (prio, (node, parent, gw), pq') = IntQueue.extract pq in
 			if !default_gw = Unix.inet_addr_any && node.gateway then
 			  default_gw := gw;
-			if IPHash.mem routes node.addr then
-			  traverse pq' (* ignore this node *)
-			else begin
+			try
+				let (existing_cost, existing_gw) = IPHash.find routes node.addr in
+				if prio == existing_cost && gw < existing_gw then begin
+					IPHash.remove routes node.addr;
+					IPHash.add routes node.addr (prio, gw);
+					if !default_gw = existing_gw then
+					  default_gw := gw;
+				end;
+				traverse pq'
+			with Not_found -> 
 				(* copy this node and hook it into the new tree *)
 				let newnode = make node.addr node.eth node.gateway [] in
 				parent.nodes <- newnode::parent.nodes;
-				IPHash.add routes node.addr gw;
+				IPHash.add routes node.addr (prio, gw);
 
 				(* Create queue elements for the children of
 				   this node and push them on. For now the
 				   priority is going to be the priority of the
 				   parent plus one, giving normal BFS
 				   behavior. *)
-				(*let prio' = prio + (if node.eth then 1 else 10) in*)
-				let prio' = prio + 1 in
+				let prio' = prio + (if node.eth then 1 else 10) in
+				(*let prio' = prio + 1 in*)
 				let pq'' = List.fold_left (fun pq' child ->
 					let child_element = (child, newnode, gw) in
 					IntQueue.insert pq' prio' child_element) pq' node.nodes in
-				traverse pq''
-			end in
+				traverse pq'' in
 	let todo = List.fold_left (fun q node ->
 			let e = (node, fake, node.addr) in
 			IntQueue.insert q 0 e) IntQueue.empty nodes in
 	traverse todo;
 
-	(* step 4 *)
-	IPHash.iter (fun a gw ->
-		if List.exists (fun (a', n) ->
-			LowLevel.route_includes_impl a' n a 32) directnets then
-		  IPHash.remove routes a) routes;
-	fake.nodes, routes, !default_gw
+	(* step 5 *)
+	let routes' = IPHash.create 512 in
+	IPHash.iter (fun a (_, gw) ->
+		if not (List.exists (fun (a', n) ->
+				LowLevel.route_includes_impl a' n a 32) directnets) then
+		  IPHash.add routes' a gw) routes;
+	fake.nodes, routes', !default_gw
 
 external serialize: node -> string = "tree_to_string"
 external deserialize: string -> node = "string_to_tree"
