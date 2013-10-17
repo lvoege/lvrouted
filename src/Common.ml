8790e5d5c5bc4c74c062352fd3e80bf045326375
lodewijk <lvoege@gmail.com>
2013-10-17 23:28:19 +0000

    double the timeout interval to see if that stabilizes the network

--- src/Common.ml
+++ src/Common.ml
@@ -12,7 +12,7 @@ let port = ref 12345
 (* How many seconds between advertising the hoptable to neighbors? *)
 let bcast_interval = ref 30.0
 (* How many seconds not hearing from a neighbor until considering it lost? *)
-let timeout: float = 4.0 *. !bcast_interval
+let timeout: float = 8.0 *. !bcast_interval
 (* How many seconds between checking interfaces and expiry times? *)
 let alarm_timeout = ref 9.0
 (* Whether or not to compress the data that goes over the wire *)
